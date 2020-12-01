56386f4458bad0f76dd8b9f713d1d861e1caddc7
josepfs62 <jfayos@cifpfbmoll.eu>
2020-12-01 17:23:05 +0100

    changes

--- inGameFunctions.py
+++ inGameFunctions.py
@@ -2,8 +2,20 @@ from cardCreationAndDistribution import randomCardDistribution
 
 randomCardDistribution()
 
-def compararCarta():
-    for carta in cardList
-        for i range(len(cardList))
+def sacarCarta():
+    for carta in cardList:
+        for i range(len(cardList)+1):
+            if (i == len(cardList)+1)
+                print(i." Pasar turno")
             print(i, end="")
         print(carta, end="")
+    int(input("Elige un número de tu mano:"))
+
+def compararCarta():
+    while (selectedcard-color != descartes[-1]) and (selectedcard-num != descartes[-1])
+        
+        print ("Elige otra carta, esta no se puede tirar")
+        #llamamos otra vez a la funcion de seleccionar carta
+
+    #if ha intentado tirar todas las cartas y no puede jugar a forzar a robar carta
+
